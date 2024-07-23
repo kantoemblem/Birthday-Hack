6d579a282b584398a4eff484ec615ccbe8abbaf1
kantoemblem <benkantor23@gmail.com>
2024-07-23 05:42:22 -0500

    Last push for Livvy bear

---
+++ Maps/Final Stronghold Tileset Forest  Update.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Final Stronghold Tileset Forest  Update" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../Complete Updated Stronghold Tileset/Final Stronghold Tileset Forest  Update.png" width="512" height="512"/>
+</tileset>
